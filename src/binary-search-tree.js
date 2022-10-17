8aa762c5222c1c89c2a14ad81f6388f7631e9865
Cuh4a <82233194+Cuh4a@users.noreply.github.com>
2022-10-17 13:10:04 +0300

    feat: finished js-task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,46 +1,193 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {
+  NotImplementedError
+} = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
+
 class BinarySearchTree {
-
+  constructor() {
+    this.rootTree = null;
+  }
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootTree;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this.rootTree = addNode(this.rootTree, data);
+
+    function addNode(node, data) {
+      if (!node) {
+        return new Node(data);
+      }
+      if (node.data === data) {
+        return node;
+      }
+      if (node.data > data) {
+        if (!node.left) {
+          node.left = new Node(data);
+          return node;
+        } else {
+          node.left = addNode(node.left, data);
+          return node;
+        }
+      }
+      if (node.data < data) {
+        if (!node.right) {
+          node.right = new Node(data);
+          return node;
+        } else {
+          node.right = addNode(node.right, data);
+          return node;
+        }
+      }
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return hasNode(this.rootTree, data);
+
+    function hasNode(node, data) {
+      if (node.data === data) {
+        return true;
+      }
+      if (node.data > data) {
+        if (node.left) {
+          return hasNode(node.left, data);
+        } else return false;
+      }
+      if (node.data < data) {
+        if (node.right) {
+          return hasNode(node.right, data);
+        } else return false;
+      }
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    return findNode(this.rootTree, data);
+
+    function findNode(node, data) {
+      if (node.data === data) {
+        return node;
+      }
+      if (node.data > data) {
+        if (node.left) {
+          return findNode(node.left, data);
+        } else return null;
+      }
+      if (node.data < data) {
+        if (node.right) {
+          return findNode(node.right, data);
+        } else return null;
+      }
+    }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    if (!this.find(data)) {
+      return;
+    }
+    let currentNode = this.find(data);
+    let parentNode = findParentNode(this.rootTree, data);
+
+
+
+    if (!currentNode.left && !currentNode.right) {
+      parentNode[0][parentNode[1]] = null;
+
+    }
+
+    if (!currentNode.left && currentNode.right) {
+      parentNode[0][parentNode[1]] = currentNode.right;
+    }
+    if (currentNode.left && !currentNode.right) {
+      parentNode[0][parentNode[1]] = currentNode.left;
+    }
+
+    if (currentNode.left && currentNode.right) {
+      let maxChild = this.max(currentNode.left);
+      let parentMaxChild = findParentNode(currentNode, maxChild);
+      console.log(currentNode, maxChild, parentMaxChild);
+      if (parentMaxChild[0]) {
+        parentMaxChild[0][parentMaxChild[1]] = null;
+      }
+      currentNode.data = maxChild;
+      console.log(maxChild, parentMaxChild);
+    }
+
+
+    function findParentNode(node, data) {
+      if (node.data === data) {
+        console.log('ya tut');
+        return [null, 'null'];
+      }
+      if (node.left) {
+        if (node.left.data === data) {
+          return [node, 'left'];
+        }
+      }
+      if (node.right) {
+        if (node.right.data === data) {
+          return [node, 'right'];
+        }
+      }
+      if (node.left) {
+        if (node.data > data) {
+          return findParentNode(node.left, data);
+        }
+      }
+      if (node.right) {
+        if (node.data < data) {
+          return findParentNode(node.right, data);
+        }
+      }
+
+    }
+
+
+
+
   }
 
-  min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  min(a = this.rootTree) {
+    if (!this.rootTree.left && !this.rootTree.right) {
+      return null;
+    }
+    let currentRoot;
+    if (typeof a === 'number') {
+      currentRoot = this.find(a);
+    } else currentRoot = a;
+
+    return findMin(currentRoot);
+
+    function findMin(node) {
+      if (node.left) {
+        return findMin(node.left);
+      } else return node.data;
+    }
   }
 
-  max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  max(a = this.rootTree) {
+    if (!this.rootTree.left && !this.rootTree.right) {
+      return null;
+    }
+    let currentRoot;
+    if (typeof a === 'number') {
+      currentRoot = this.find(a);
+    } else currentRoot = a;
+
+    return findMax(currentRoot);
+
+    function findMax(node) {
+      if (node.right) {
+        return findMax(node.right);
+      } else return node.data;
+    }
   }
 }
 
